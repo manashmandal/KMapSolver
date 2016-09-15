ff566fa6d621189f19b8f72d7047175b260d840f
Manash Kumar Mandal <manashmndl@gmail.com>
2016-09-15 17:56:31 +0600

    variable counter

--- Console/KMapSolver/expressionhandler.h
+++ Console/KMapSolver/expressionhandler.h
@@ -4,6 +4,8 @@
 #include "include/Lepton.h"
 #include <QString>
 #include <string>
+#include <set>
+
 
 using std::string;
 using std::vector;
@@ -16,6 +18,30 @@ string getInvertedExpression(char variable)
 {
     string var(1, variable);
     return "(1-" + var + ")";
+}
+
+unsigned int countVariable(QString expression)
+{
+    set <char> charSet;
+    for (char c : expression){
+        if (!ispunct(c) && !isdigit(c))
+            charSet.insert(charSet.begin(), c);
+    }
+    set <char>::iterator it = charSet.end();
+    it--;
+    return ((*it) - 'A' + 1);
+}
+
+unsigned int countVariable(std::string expression)
+{
+    set <char> charSet;
+    for (char c : expression.toStdString()){
+        if (!ispunct(c) && !isdigit(c))
+            charSet.insert(charSet.begin(), c);
+    }
+    set <char>::iterator it = charSet.end();
+    it--;
+    return ((*it) - 'A' + 1);
 }
 
 std::string parseExpression(std::string expr)
